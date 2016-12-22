6bbb20b59acc733bedab739775fdcb07f7b2adee
aki2k16 <kobayashi.a1987@tbz.t-com.ne.jp>
2016-12-22 17:42:21 +0900

    Specify actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "groups#index"
-  resources :groups 
-  resources :messages
+  resources :groups, only: [:index, :new, :create, :show, :edit, :update] 
+  resources :messages, only: [:index] 
 end
