67246eb53e1f3bd1bcd5b3829ba4b19d66100641
aki2k16 <kobayashi.a1987@tbz.t-com.ne.jp>
2016-12-22 19:41:41 +0900

    Write uncreating test file code.

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module ChatSpace
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ja
 
+    config.generators do |g|
+    g.helper false
+    g.assets false
+    g.test_framework false
+   end
   end
 end
