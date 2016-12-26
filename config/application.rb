6b86d9515e60a229ef4e97e5ee32541399962502
aki2k16 <kobayashi.a1987@tbz.t-com.ne.jp>
2016-12-27 02:12:20 +0900

    Change time zone.

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,10 @@ module ChatSpace
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ja
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+
     config.generators do |g|
     g.helper false
     g.assets false
