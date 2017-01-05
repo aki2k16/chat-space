70fdb006d74eb0446ef4ae9deb6841274c47fa6a
aki2k16 <kobayashi.a1987@tbz.t-com.ne.jp>
2017-01-05 13:14:43 +0900

    Add comments.

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 describe MessagesController do
-    let(:user) {create(:user)}
-    let(:group) {create(:group)}
-    let(:message) {create(:message)}
+  let(:group) {create(:group)}
+  let(:message) {create(:message)}
+  let(:user) {create(:user)}
 
-    before do
-      sign_in user
-    end
-    after do
-      sign_out user
-    end
+
+  before do
+    # サインイン
+    sign_in user
+  end
+
+   after do
+    sign_out user
+  end
+
   describe 'GET #index' do
     let(:groups) {create_list(:group, 5, user_ids: user.id)}
     before do
@@ -32,29 +36,28 @@ describe MessagesController do
   end
 
   describe 'Post #create' do
-    let(:message) {build(:message)}
-
+    let(:messages) {build(:messages)}
+    #データが登録できた場合。
     it "save success" do
       expect {
         post :create, group_id: group.id, message: attributes_for(:message)
       }.to change(Message, :count).by(1)
     end
-
+    #トップ画面への遷移。
     it "redirect to :index" do
       post :create, group_id: group.id, message: attributes_for(:message)
       expect(response).to redirect_to group_messages_path
     end
-
+    #データベースへ保存できない場合。
     it "can not save in the database" do
       expect{
         post :create, group_id: group.id, message: attributes_for(:message, body: nil)
       }.not_to change(Message, :count)
     end
-
+    #トップ画面への遷移
     it "redirects to messages#index" do
       post :create, group_id: group.id, message: attributes_for(:message, body: nil)
       expect(response).to redirect_to group_messages_path
     end
   end
-
 end
